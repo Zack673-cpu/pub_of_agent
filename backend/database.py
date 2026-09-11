36062ab2b8328f46d535ddd2fa71c0976a7010f1
爱 Zack <3047981689@qq.com>
2026-09-11 14:11:22 +0800

    Merge worktree branch feat-fullstack-learning-wMOcne

--- backend/database.py
+++ backend/database.py
@@ -10,33 +10,7 @@ import sys
 load_dotenv()
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s",
-    handlers=[
-        logging.StreamHandler(),           # 终端
-        logging.FileHandler(Path(__file__).parent/"app.log", encoding="utf-8"),  # 文件
-    ]
-)
-logger = logging.getLogger(__name__)
 
-
-
-password=os.getenv("DB_PASSWORD")
-if password is None:
-    logger.error("    DB_PASSWORD 为空，检查 .env 文件")
-    sys.exit()
-
-
-
-
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': password,  # 若无密码则留空 ''
-    'port': 3306,
-    'charset': 'utf8mb4'
-}
 
 db_name="textbook_agent"
 
