2eb93c85997fe6495deaca2e1f086738c792c5ac
爱 Zack <3047981689@qq.com>
2026-09-14 18:51:46 +0800

    完整登录收尾

--- rag/app.py
+++ rag/app.py
@@ -1,18 +1,19 @@
 import rag.db as db
 import rag.rag as rag
 from fastapi import FastAPI,Depends,Header,HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from fastapi.staticfiles import StaticFiles
 import rag.pipeline as pipeline
 import logging
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-from backend.database import get_session, SessionLocal
+from backend.database import get_session
 from sqlalchemy.orm import Session
-from rag.auth import hash_refresh_token,create_refresh_token_plain,create_access_token
+from rag.auth import hash_refresh_token,create_refresh_token_plain,create_access_token,hash_password,verify_password
 from backend.models import RefreshToken, User
 from datetime import datetime,timezone,timedelta
+from sqlalchemy.exc import IntegrityError
 
 
 
@@ -59,7 +60,12 @@ class SECTION(BaseModel):
 
 class RefreshBody(BaseModel):
     refresh_token: str
-
+class RegisterBody(BaseModel):
+    username:str=Field(...,min_length=3,max_length=20)
+    password:str=Field(...,min_length=5)
+class LoginBody(BaseModel):
+    username:str
+    password:str
 
 @app.post("/ask")
 async def answer_agent(question:QUESTION,authorization: str=Header(default=None)):
@@ -114,7 +120,7 @@ async def generate_agent(section:SECTION,authorization: str=Header(default=None)
 
 @app.post("/refresh")
 async def refresh_token(body: RefreshBody, db: Session = Depends(get_session)):
-    
+    #这个函数直接换两种token
     #db：参数名。
     #Session：类型注解，表示你期望 db 是 Session 类型。
     #Depends(get_session)：告诉 FastAPI，这个参数不要从请求里解析，而是调用 get_session 来获取。
@@ -156,3 +162,76 @@ async def refresh_token(body: RefreshBody, db: Session = Depends(get_session)):
             raise HTTPException(status_code=500,detail=str(e))
         
         return {"access_token":new_access,"refresh_token":refresh_plain}
+
+
+@app.post("/register")
+async def register(body:RegisterBody,db:Session=Depends(get_session)):
+    #查重，插入新用户，发双token，其中refresh进库
+    check=db.query(User).filter(User.username==body.username).first()
+    if check is not None:
+        raise HTTPException(status_code=400,detail="   用户名已经被使用")
+    else:            
+        password_hashed=hash_password(body.password)
+        new_row=User(
+            username=body.username,
+            password_hash=password_hashed,
+        )
+        try:
+            db.add(new_row)
+            db.commit()
+        except IntegrityError:
+            db.rollback()
+            raise HTTPException(status_code=400, detail="   用户名已经被使用")
+
+        now=datetime.now(timezone.utc)
+        userid=new_row.id
+        access_token=create_access_token(userid)
+        refresh_token=create_refresh_token_plain()
+        refresh_token_hashed=hash_refresh_token(refresh_token)
+
+        try:
+            new_row=RefreshToken(
+                user_id=userid,
+                token=refresh_token_hashed,
+                expires_at=now+timedelta(days=7),
+                revoked=False,
+            )
+            db.add(new_row)
+            db.commit()
+        except Exception as e:
+            raise HTTPException(status_code=500,detail=str(e))
+        
+        return {"message":"注册成功","access_token":access_token,"refresh_token":refresh_token}
+
+
+
+@app.post("/login")
+async def login(body:LoginBody,db:Session=Depends(get_session)):
+    user=db.query(User).filter(User.username==body.username).first()
+    if user is None:
+        raise HTTPException(status_code=401,detail="    用户名或密码错误")
+    else:
+        check=verify_password(body.password,user.password_hash)
+        if check is False:
+            raise HTTPException(status_code=401,detail="    用户名或密码错误")
+        now=datetime.now(timezone.utc)
+        userid=user.id
+        access_token=create_access_token(userid)
+        refresh_token=create_refresh_token_plain()
+        refresh_token_hashed=hash_refresh_token(refresh_token)
+
+        try:
+            new_row=RefreshToken(
+                user_id=userid,
+                token=refresh_token_hashed,
+                expires_at=now+timedelta(days=7),
+                revoked=False,
+            )
+            db.add(new_row)
+            db.commit()
+        except Exception as e:
+            raise HTTPException(status_code=500,detail=str(e))
+        
+        return {"message":"登录成功","access_token":access_token,"refresh_token":refresh_token}
+
+    
